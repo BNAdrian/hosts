e8acc51a23958bf498d8c7effaf458b2c4745370
BNAdrian <nicolae-adrian.balaban@s.unibuc.ro>
2025-12-17 12:36:56 +0200

    Commit 2: am rectificat scriptul pt a adauga functie

---
+++ check_cu_funtie.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+check_ip() {
+        hosts=$1
+        ip=$2
+        dns=$3
+
+        real_ip=$(nslookup $hosts $dns | grep Adress | tail -n 1| awk '{print $2}')
+        if [[ "$real_ip" != "$ip" ]]; then
+                echo "Bogus IP for $host in /etc/hosts !"
+        fi
+}
+
+cat /etc/hosts | while read ip name rest
+do
+        if [[ "$ip" =~ ^[0-9] ]]; then
+                check_ip "$name" "$ip" "8.8.8.8"
+        fi
+done
